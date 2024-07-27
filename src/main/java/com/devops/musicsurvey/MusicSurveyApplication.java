3b583595c3bfcb7396b78ac5b4ba4a4110cc9bf2
BillyH4k <billyjag1@gmail.com>
2024-07-27 15:59:51 -0400

    first commit

---
+++ src/main/java/com/devops/musicsurvey/MusicSurveyApplication.java
@@ -0,0 +1,13 @@
+package com.devops.musicsurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSurveyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicSurveyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devops/musicsurvey/MusicSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.musicsurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSurveyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
